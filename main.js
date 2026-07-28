ab8f2b171b2951ee61f24c44817c5916d57a1b08
Ky0y4 <88522509+Ky0y4@users.noreply.github.com>
2026-07-28 17:08:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ await new Promise(resolve => loader.load(resolve));
 
 const camera = new Entity('Camera');
 
-camera.setPosition(0.3, 0.7, 2.5);
+camera.setPosition(0.5, 2, 2.5);
 
 camera.addComponent('camera', {
     clearColor: [0, 0, 0, 0]
