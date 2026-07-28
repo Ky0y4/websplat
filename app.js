82dc81eba19df4da2b7d8bfc8323a73014f51298
Rex Leong <ky0y4z1n@gmail.com>
2026-07-28 11:38:39 +0100

    convert to playcanvas

--- app.js
+++ app.js
@@ -1,76 +1,29 @@
-const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas, true);
+import { Application, Asset, AssetListLoader, Entity, FILLMODE_FILL_WINDOW, RESOLUTION_AUTO } from 'playcanvas';
 
-let splatRoot, camera, xr;
+// Create application
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
 
-const createScene = async function () {
-    const scene = new BABYLON.Scene(engine);
-
-    camera = new BABYLON.ArcRotateCamera(
-        "camera",
-        -Math.PI / 2,
-        Math.PI / 2.5,
-        3,
-        BABYLON.Vector3.Zero(),
-        scene
-    );
-    camera.attachControl(canvas, true);
-
-    const light = new BABYLON.HemisphericLight(
-        "light",
-        new BABYLON.Vector3(0, 1, 0),
-        scene
-    );
-
-   
-    splatRoot = new BABYLON.TransformNode("splatRoot", scene);
-    const splat = new BABYLON.GaussianSplattingMesh("splat", null, scene);
-    splat.parent = splatRoot;
-    await splat.loadFileAsync("./vrlabdensed100k.sog");
-
-    xr = await scene.createDefaultXRExperienceAsync({
-        uiOptions: {
-            sessionMode: "immersive-ar"
-        },
-        optionalFeatures: true,
-        disableTeleportation: true
-    });
-
-    scene.activeCamera = xr.baseExperience.camera;
-
-  
-    xr.baseExperience.onStateChangedObservable.add((state) => {
-        if (state === BABYLON.WebXRState.IN_XR) {
-            camera.detachControl();
-        } else if (state === BABYLON.WebXRState.NOT_IN_XR) {
-            camera.attachControl(canvas, true);
-        }
-    });
-
-
-    xr.input.onControllerAddedObservable.add((controller) => {
-        controller.onMotionControllerInitObservable.add((motionController) => {
-            const trigger = motionController.getComponent("xr-standard-trigger");
-            if (trigger) {
-                trigger.onButtonStateChangedObservable.add(() => {
-                    if (trigger.pressed) {
-                        splatRoot.position.copyFrom(xr.baseExperience.camera.position);
-                    }
-                });
-            }
-        });
-    });
-
-    return scene;
-};
-
-(async () => {
-    const scene = await createScene();
-    engine.runRenderLoop(function () {
-        scene.render();
-    });
-})();
-
-window.addEventListener("resize", function () {
-    engine.resize();
+const app = new Application(canvas, {
+    graphicsDeviceOptions: {
+        antialias: false
+    }
 });
+app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
+app.setCanvasResolution(RESOLUTION_AUTO);
+app.start();
+
+window.addEventListener('resize', () => app.resizeCanvas());
+
+//load assets
+const assets = [
+    new Asset('camera-controls', 'script', {
+        url: 'https://cdn.jsdelivr.net/npm/playcanvas/scripts/esm/camera-controls.mjs'
+    }),
+    new Asset('toy', 'gsplat', {
+        url: 'https://developer.playcanvas.com/assets/toy-cat.sog'
+    })
+];
+
+const loader = new AssetListLoader(assets, app.assets);
+await new Promise(resolve => loader.load(resolve));
