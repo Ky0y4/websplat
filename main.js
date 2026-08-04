b148a19822dd99f435bf3cb6933622866f980e6c
Rex Leong <ky0y4z1n@gmail.com>
2026-08-04 12:03:18 +0100

    retry rays

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ import {
     XRTYPE_AR,
     XRSPACE_LOCALFLOOR,
     Vec3,
-    Color
+    Color,
+    StandardMaterial
 } from 'playcanvas';
 
 // ----------------------------------------------------
@@ -172,6 +173,17 @@ app.xr.input.on("add", (inputSource) => {
     camera.addChild(entity);
     activeControllers.push(entity);
 
+    const rayEntity = new Entity('Ray');
+    rayEntity.addComponent('render', { type: 'cylinder' });
+    rayEntity.setLocalScale(0.005, 0.005, 5);
+    const rayMat = new StandardMaterial();
+    rayMat.emissive = new Color(1, 1, 1);
+    rayMat.update();
+    rayEntity.render.meshInstances[0].material = rayMat;
+    entity.addChild(rayEntity);
+    entity._rayEntity = rayEntity;
+    entity._rayMat = rayMat;
+
     const profileId = inputSource.profiles[0];
     const handedness = inputSource.handedness;
     const url = `https://cdn.jsdelivr.net/npm/@webxr-input-profiles/assets@1.0/dist/profiles/${profileId}/${handedness}.glb`;
@@ -207,7 +219,6 @@ const forward = new Vec3();
 const move = new Vec3();
 const target = new Vec3();
 
-
 // ----------------------------------------------------
 // UPDATE
 // ----------------------------------------------------
@@ -216,12 +227,26 @@ app.on("update", (dt) => {
 
     for (const entity of activeControllers) {
         const src = entity._inputSource;
-        if (src.grip) {
+
+        const pos = src.getPosition();
+        const rot = src.getRotation();
+
+        if (src.grip && pos && rot) {
             entity.enabled = true;
-            entity.setPosition(src.getPosition());
-            entity.setRotation(src.getRotation());
+            entity.setPosition(pos);
+            entity.setRotation(rot);
         } else {
             entity.enabled = false;
+        }
+
+        const ray = entity._rayEntity;
+        if (ray) {
+            ray.setLocalEulerAngles(90, 0, 0);
+            ray.setLocalPosition(0, 0, -2.5);
+
+            const isSelecting = src.selecting || (src.gamepad && src.gamepad.buttons[0] && src.gamepad.buttons[0].pressed);
+            entity._rayMat.emissive.set(isSelecting ? 0 : 1, 1, isSelecting ? 0 : 1);
+            entity._rayMat.update();
         }
     }
 
